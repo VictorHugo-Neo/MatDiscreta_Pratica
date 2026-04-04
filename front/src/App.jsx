23efa0a28060edcb2a6d1a6e08ce1160d6fac107
VictorHugo-Neo <prfvhlc@gmail.com>
2026-04-03 23:17:14 -0300

    Add monaco editor e uma interface básica

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,15 +1,25 @@
 import {useState} from 'react'
+import Editor from "@monaco-editor/react"
 
 function App(){
   const [carregando, setCarregando] = useState(true)
 
+  const [codigo, setCodigo] = useState("# Digite seu código Python aqui..")
+  const [saida, setSaida] = useState(">O resultado do cálculo aparecerá aqui...")
+
+  const executarCodigo = () => {
+    setSaida(`>Executando código...\n${codigo}\n>Resultado: 42`)
+
+  }
   return (
     <div className = "min-h-screen bg-gray-900 text-gray-100 font-sans p-8">
       <div className = "max-w-4xl mx-auto space-y-6">
+        
         <header className = "text-center">
           <h1 className = "text-3xl font-bold text-blue-400">Laboratório de Cálculo Numérico</h1>
           <p className = "text-gray-400 mt-2">Semana 3: Comandos Básicos e Implementação</p>
         </header>
+        
         <div className="bg-gray-800 p-6 rounded-lg shadow-lg border border-gray-700">
 
           <div className="mb-4 text-sm font-medium">
@@ -17,20 +27,34 @@ function App(){
             <span className = "text-yellow-400">Aguardando conexão...</span>
           </div>
 
-          <div className = "h-64 bg-gray-950 rounded border border-gray-600 flex items-center justifiy-center mb-4">
-            <span className="text-gray-500">Digite o seu código aqui</span>
+          <div className = "h-64 rounded border border-gray-600 mb-4 overflow-hidden">
+            <Editor
+              height='100%'
+              defaultLanguage='python'
+              theme='vs-dark'
+              value={codigo}
+              onChange={(value) => setCodigo(value || '')}
+              options={{
+                minimap: { enabled: false},
+                fontSize: 16,
+                padding: {top: 16}
+              }}
+            />
           </div>
-          <button className ="w-full bg-blue-600 hover:bg-blue-500 text-white font-bold py-3 px-4 rounded transition-colors disabled:opacity-50">
+          <button
+            onClick={executarCodigo}
+            className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded transition duration-200"
+          >
             Executar Código
           </button>
         </div>
 
-        <div classNmae = "bg-black p-4 rounded-lg border border-gray-700 h-32 overflow-y-auto font-mono text-sm text-green-400">
-          <p>{">"} O resultado do cálculo é....</p>
+        <div className = "bg-black p-4 rounded-lg border border-gray-700 h-48 overflow-y-auto font-mono text-sm text-green-400 whitespace-pre-wrap">
+          {saida}
         </div>
       
       </div>
     </div>
   )
 }
-export default App 
+export default App
